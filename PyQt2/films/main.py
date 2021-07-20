e66e69c0a167f79f9da5aa94b2027472ad8cdec1
STR WATCHER <reddreadthomaas@yandex.ru>
2021-07-20 22:17:56 +1000

    A, B, C, D and E tasks of day 2 solved

---
+++ PyQt2/films/main.py
@@ -0,0 +1,242 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import Qt
+import sys
+import sqlite3
+from functools import partial
+
+FIELDS = ('Идентификатор', 'Название', 'Год', 'Жанр', 'Продолжительность')
+DB_FIELDS = ('id', 'title', 'year', 'genre', 'duration')
+ID_POSITION = 0
+GENRE_POSITION = 3
+
+
+class MainWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.table_widget = TableWidget(self)
+        self.form_widget = NewFilmForm(self)
+        self.stacked_widget = QStackedWidget()
+
+        self.setup_ui()
+        self.show()
+
+    def setup_ui(self):
+        layout = QVBoxLayout()
+        self.setGeometry(200, 200, 1000, 1000)
+
+        self.stacked_widget.addWidget(self.table_widget)
+        self.stacked_widget.addWidget(self.form_widget)
+        self.stacked_widget.setCurrentWidget(self.table_widget)
+
+        layout.addWidget(self.stacked_widget)
+        self.setLayout(layout)
+
+    def switch_to_form(self):
+        self.stacked_widget.setCurrentWidget(self.form_widget)
+
+    def switch_to_table(self):
+        self.stacked_widget.setCurrentWidget(self.table_widget)
+
+
+class TableWidget(QWidget):
+    def __init__(self, parent):
+        super().__init__()
+        self.parent = parent
+        self.combobox = QComboBox()
+        self.table = FilmsTable()
+
+        self.save_button = QPushButton("Сохранить")
+        self.add_button = QPushButton("Добавить")
+        self.setup_ui()
+
+    def setup_ui(self):
+        main_layout = QVBoxLayout()
+        self.combobox.addItems(FIELDS)
+        self.combobox.activated[str].connect(self.chosen_field_catcher)
+        self.table.setMinimumSize(800, 800)
+        self.save_button.clicked.connect(self.table.connection.commit)
+        self.add_button.clicked.connect(self.parent.switch_to_form)
+        main_layout.addWidget(self.combobox)
+        main_layout.addWidget(self.add_button)
+        main_layout.addWidget(self.table)
+        main_layout.addWidget(self.save_button)
+        self.setLayout(main_layout)
+
+    def chosen_field_catcher(self, text):
+        if text in ('Идентификатор', 'Год', 'Продолжительность'):
+            i, ok_pressed = QInputDialog.getInt(self, text, 'Введите {} для выборки'.format(text))
+            if ok_pressed:
+                self.table.search_in_table(text, i)
+
+        elif text in ('Жанр', 'Название'):
+            i, ok_pressed = QInputDialog.getText(self, text, 'Введите {} для выборки'.format(text))
+            if ok_pressed:
+                self.table.search_in_table(text, i)
+
+    def add(self, data):
+        self.table.add(data)
+
+
+class FilmsTable(QWidget):
+    def __init__(self, db_name='films.db'):
+        super().__init__()
+        self.connection = sqlite3.connect(db_name)
+        self.cursor = self.connection.cursor()
+        self.table = QTableWidget()
+        self.setup_ui()
+
+    def setup_ui(self):
+        layout = QVBoxLayout()
+        layout.addWidget(self.table)
+        self.setLayout(layout)
+
+    def search_in_table(self, field, value):
+
+        films = self.make_the_request(field, value)
+
+        if len(films) > 0:
+            self.table.setColumnCount(len(films[0]))
+
+        self.table.setHorizontalHeaderLabels(FIELDS)
+        self.table.setRowCount(0)
+        for i, row in enumerate(films):
+            self.table.setRowCount(self.table.rowCount() + 1)
+            for j, item in enumerate(row):
+                if j == GENRE_POSITION:
+                    item = list(self.cursor
+                                .execute("""SELECT title FROM genres WHERE id=?""", (item,))
+                                .fetchone()) \
+                        .pop()
+                item = QTableWidgetItem(str(item))
+                if j == ID_POSITION:
+                    item.setFlags(item.flags() ^ Qt.ItemIsEditable)
+                self.table.setItem(i, j, item)
+
+        self.table.itemChanged.connect(self.changes_catcher)
+
+        self.table.resizeColumnsToContents()
+
+    def make_the_request(self, field, value, quantity=30):
+        relations = {
+            'Идентификатор': """SELECT * FROM FILMS WHERE id = ?""",
+            'Название': """SELECT * FROM FILMS WHERE title like ?""",
+            'Год': """SELECT * FROM FILMS WHERE year = ?""",
+            'Продолжительность': """SELECT * FROM FILMS WHERE duration = ?""",
+            'Жанр': """SELECT * FROM FILMS
+                            WHERE genre=(
+                        SELECT id FROM genres
+                            WHERE title like ?)"""
+        }
+
+        if field == 'Название':
+            value += '%'
+
+        return self.cursor.execute(relations[field], (value,)).fetchmany(quantity)
+
+    def changes_catcher(self, item: QTableWidgetItem):
+        film_id = int(self.table.item(item.row(), 0).text())
+        field = list(DB_FIELDS)[item.column()]
+        request = """UPDATE Films
+                  SET {} = ?
+                  WHERE id = {}""".format(field, film_id)
+
+        if item.column() in (2, 4):
+            try:
+                self.cursor.execute(request, (int(item.text()),))
+            except:
+                self.cursor.execute(request, (0, ))
+        elif item.column() == 1:
+            self.cursor.execute(request, (item.text(),)).fetchone()
+        elif item.column() == 3:
+            wrapped_id_of_genre = self.cursor.execute("""SELECT id
+                                                         FROM genres
+                                                         WHERE title = ?""", (item.text(),)).fetchone()
+
+            if not wrapped_id_of_genre:
+                # magic number because айдишники в исходной бд немножко плывут на 2 ровно
+                count = list(self.cursor.execute("""SELECT COUNT(*) FROM genres""").fetchone())[0] + 3
+                self.cursor.execute("""INSERT INTO genres(id,title) VALUES(?, ?)""", (count, item.text()))
+                self.cursor.execute("""UPDATE Films
+                                        SET genre = ?
+                                        WHERE id = {}""".format(film_id), (count,))
+
+            else:
+                self.cursor.execute("""UPDATE Films
+                                       SET genre = ?
+                                       WHERE id = {}""".format(film_id), (list(wrapped_id_of_genre)[0],))
+
+    def add(self, data):
+        id_of_genre = self.cursor.execute("""SELECT id
+                                             FROM genres
+                                             WHERE title = ?""", (data['genre'],)).fetchone()
+
+        if not id_of_genre:
+            id_of_genre = list(self.cursor.execute("""SELECT COUNT(*) FROM genres""").fetchone())[0] + 3
+            self.cursor.execute("""INSERT INTO genres(id,title) VALUES(?, ?)""", (id_of_genre, data['genre']))
+        else:
+            id_of_genre = list(id_of_genre)[0]
+
+        # another one magic number
+        film_id = list(self.cursor.execute("""SELECT COUNT(*) FROM Films""").fetchone())[0] + 1000
+
+        self.cursor.execute("""INSERT INTO Films(id,title,year,genre,duration) VALUES(?,?,?,?,?)""",
+                            (film_id, data["title"], data["year"], id_of_genre, data["duration"]))
+
+        self.connection.commit()
+        item = QTableWidgetItem(film_id)
+        item.setFlags(item.flags() ^ Qt.ItemIsEditable)
+
+        self.search_in_table(FIELDS[1], data["title"])
+
+
+class NewFilmForm(QWidget):
+    def __init__(self, parent):
+        super().__init__()
+        self.parent = parent
+        self.title = QLineEdit()
+        self.genre = QLineEdit()
+        self.duration = QSpinBox()
+        self.duration.setMaximum(1000)
+        self.year = QSpinBox()
+        self.year.setMaximum(2021)
+        self.submit_button = QPushButton("Добавить")
+        self.cancel_button = QPushButton("Отмена")
+
+        self.setup_ui()
+
+    def setup_ui(self):
+        layout = QVBoxLayout()
+        self.submit_button.clicked.connect(self.add)
+        self.cancel_button.clicked.connect(self.parent.switch_to_table)
+        layout.addWidget(QLabel("Название"))
+        layout.addWidget(self.title)
+        layout.addWidget(QLabel("Жанр"))
+        layout.addWidget(self.genre)
+        layout.addWidget(QLabel("Год"))
+        layout.addWidget(self.year)
+        layout.addWidget(QLabel("Продолжительность"))
+        layout.addWidget(self.duration)
+        layout.addWidget(self.submit_button)
+        layout.addWidget(self.cancel_button)
+
+        self.setLayout(layout)
+
+    def add(self):
+        res = {
+            'title': self.title.text(),
+            'genre': self.genre.text(),
+            'year': self.year.value(),
+            'duration': self.duration.value()
+        }
+        self.parent.table_widget.add(res)
+        self.year.setValue(0)
+        self.duration.setValue(0)
+        self.title.setText("")
+        self.genre.setText("")
+        self.parent.switch_to_table()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MainWidget()
+    sys.exit(app.exec())
