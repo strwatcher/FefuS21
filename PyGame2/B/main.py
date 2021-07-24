a7b1a482fd663b0b623e3895fbd39c12c6682a0a
STR WATCHER <reddreadthomaas@yandex.ru>
2021-07-24 18:50:38 +1000

    PyGame2: task B solution

---
+++ PyGame2/B/main.py
@@ -0,0 +1,137 @@
+import os
+import pygame
+pygame.init()
+
+display = pygame.display.set_mode(size=(0, 0), flags=pygame.FULLSCREEN)
+
+
+def get_index(x, y, level_width):
+    return y * level_width + x
+
+
+def load_level(filename):
+    filename = "data/" + filename
+    with open(filename, 'r') as level:
+        level_map = [line.strip() for line in level]
+
+    max_width = max(map(len, level_map))
+
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
+def load_image(name, color_key=None):
+    fullname = os.path.join('res', name)
+    try:
+        image = pygame.image.load(fullname).convert()
+    except pygame.error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+
+    if color_key is not None:
+        if color_key == -1:
+            color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+tile_images = {
+    'wall': load_image('wall.png'),
+    'grass': load_image('grass.png')
+}
+
+player_sprite = load_image('minotaur.png', -1)
+
+tile_width = tile_height = 100
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+walls_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        if tile_type == 'wall':
+            super().__init__(tiles_group, all_sprites, walls_group)
+        else:
+            super().__init__(tiles_group, all_sprites)
+        self.image = pygame.transform.scale(tile_images[tile_type], (100, 100))
+        self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
+        self.pos = pos_x, pos_y
+        self.tile_type = tile_type
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.image = pygame.transform.scale(player_sprite, (100, 100))
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x + 15, tile_height * pos_y + 5)
+        self.vision = 1, 0
+        self.pos = pos_x, pos_y
+
+    def move(self, _velocity):
+        v_x, v_y = _velocity
+        if v_x != 0 or v_y != 0:
+            self.vision = _velocity
+            m_x, m_y = self.pos
+            m_x += v_x
+            m_y += v_y
+
+            if not tiles_group.sprites()[get_index(m_x, m_y, level_x + 1)] in walls_group:
+                self.pos = m_x, m_y
+                x, y = self.rect.x + v_x * tile_width, self.rect.y + v_y * tile_height
+                self.rect = self.image.get_rect().move(x, y)
+
+    def attack(self):
+        vision_x, vision_y = self.vision
+        m_x, m_y = self.pos
+        attack_x, attack_y = vision_x + m_x, vision_y + m_y
+        attacked_tile = tiles_group.sprites()[get_index(attack_x, attack_y, level_x + 1)]
+        if attacked_tile in walls_group:
+            walls_group.remove(attacked_tile)
+            attacked_tile.image = pygame.transform.scale(tile_images['grass'], (tile_width, tile_height))
+
+
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level)):
+            if level[y][x] == '.':
+                Tile('grass', x, y)
+            elif level[y][x] == '#':
+                Tile('wall', x, y)
+            elif level[y][x] == '@':
+                Tile('grass', x, y)
+                new_player = Player(x, y)
+    return new_player, x, y
+
+
+player, level_x, level_y = generate_level(load_level('map.txt'))
+running = True
+velocity = 0, 0
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP:
+                velocity = 0, -1
+            elif event.key == pygame.K_DOWN:
+                velocity = 0, 1
+            elif event.key == pygame.K_RIGHT:
+                velocity = 1, 0
+            elif event.key == pygame.K_LEFT:
+                velocity = -1, 0
+            elif event.key == pygame.K_SPACE:
+                player.attack()
+
+    player.move(velocity)
+    velocity = 0, 0
+    tiles_group.draw(display)
+    player_group.draw(display)
+    pygame.display.flip()
+
+pygame.quit()
