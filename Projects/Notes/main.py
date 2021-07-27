eb60b0e2b2d37d1a1a5f6b1a9b09832614e29904
STR WATCHER <reddreadthomaas@yandex.ru>
2021-07-27 15:19:06 +1000

    NOtes: add possibility to delete notes

--- Projects/Notes/main.py
+++ Projects/Notes/main.py
@@ -2,8 +2,33 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QCloseEvent
 from notes_ui import Ui_main_widget
+from typing import *
 import sqlite3
 import sys
+
+
+NOTES_START_INDEX = 2
+
+
+def delete_button_from_list(note_id, buttons_list: List[QPushButton]):
+    for i, button in enumerate(buttons_list):
+        if int(button.whatsThis()) == note_id:
+            buttons_list.pop(i)
+
+
+def search_button(note_id, buttons_list: List[QPushButton]) -> QPushButton:
+    for button in buttons_list:
+        if int(button.whatsThis()) == note_id:
+            return button
+
+
+def clear_layout(layout: QLayout, start_index: int):
+    while layout.count() > start_index:
+        item = layout.itemAt(start_index)
+        widget = item.widget()
+        layout.removeItem(item)
+        layout.removeWidget(widget)
+        widget.setParent(QWidget())
 
 
 class MainWidget(QWidget):
@@ -16,7 +41,7 @@ class MainWidget(QWidget):
         self.connection = sqlite3.connect(db_name)
         self.cursor = self.connection.cursor()
         self.opened_notes = dict()
-        self.notes = list()
+        self.notes_buttons_list = list()
         self.next_id = 1
         self.read_all_notes()
         self.show()
@@ -28,16 +53,18 @@ class MainWidget(QWidget):
         self.ui.create_note_button.clicked.connect(self.open_note)
         self.ui.menu_button.clicked.connect(self.hide_menu)
         self.ui.menu_button_layout.setAlignment(Qt.AlignLeft)
+        empty_edit_area = EmptyEditArea()
+        self.ui.edit_area.addWidget(empty_edit_area)
+        self.ui.edit_area.setCurrentWidget(empty_edit_area)
+        self.ui.delete_button = QPushButton("🗑")
+        self.ui.delete_button.clicked.connect(self.delete_note)
+        self.ui.tool_bar_layout.addWidget(self.ui.delete_button, alignment=Qt.AlignRight)
 
     def closeEvent(self, a0: QCloseEvent) -> None:
         for key, value in self.opened_notes.items():
-            if value.update:
-                request = """UPDATE Notes
+            request = """UPDATE Notes
                          SET header = "{}", body = "{}"
                          WHERE id = {}""".format(value.header_view.text(), value.body_view.toPlainText(), key)
-            else:
-                request = """INSERT INTO Notes(id, header, body) VALUES({}, "{}", "{}")"""\
-                          .format(key, value.header_view.text(), value.body_view.toPlainText())
 
             self.cursor.execute(request)
 
@@ -46,53 +73,98 @@ class MainWidget(QWidget):
     def read_all_notes(self):
         request = """SELECT * FROM Notes"""
         response = self.cursor.execute(request).fetchall()
-        self.next_id = len(response) + 1
         for note in response:
             note = list(note)
             note_button = QPushButton(note[1])
             note_button.setWhatsThis("{}".format(note[0]))
             note_button.clicked.connect(self.open_note)
-            self.ui.notes_list_layout.insertWidget(1, note_button)
-            self.notes.append(note_button)
+            self.ui.notes_list_layout.insertWidget(NOTES_START_INDEX, note_button)
+            self.notes_buttons_list.append(note_button)
+
+            if note[0] > self.next_id:
+                self.next_id = note[0]
+
+        self.next_id += 1
+
+    def update_notes_list_view(self):
+        for note_button in self.notes_buttons_list:
+            self.ui.notes_list_layout.insertWidget(NOTES_START_INDEX, note_button)
 
     def open_note(self):
         note_id = int(self.sender().whatsThis())
         if note_id not in self.opened_notes.keys():
             if note_id == 0:
-                note_button = QPushButton()
-                note_button.setWhatsThis("{}".format(self.next_id))
-                note_widget = Note(note_button, note_id=self.next_id)
-                print(self.next_id)
+                note_id = self.next_id
                 self.next_id += 1
+
+                note_button = QPushButton()
                 note_button.clicked.connect(self.open_note)
-                self.ui.notes_list_layout.insertWidget(1, note_button)
-                self.notes.append(note_button)
+
+                note_widget = Note(note_button, note_id)
+
+                self.ui.notes_list_layout.insertWidget(NOTES_START_INDEX, note_button)
+                self.notes_buttons_list.append(note_button)
+
+                request = """INSERT INTO Notes(id, header, body) Values({}, '', '')""".format(note_id)
+                self.cursor.execute(request)
+
+                self.connection.commit()
 
             else:
                 request = """SELECT * FROM Notes WHERE id=?"""
-                response = self.cursor.execute(request, (note_id, )).fetchone()
-                note_button = self.ui.notes_list_layout.itemAt(self.next_id - note_id).widget()
+                response = self.cursor.execute(request, (note_id,)).fetchone()
+
+                note_button = search_button(note_id, self.notes_buttons_list)
                 note_widget = Note(note_button, note_id, response[1], response[2], True)
 
+            note_button.setWhatsThis("{}".format(note_id))
             self.ui.edit_area.addWidget(note_widget)
             self.opened_notes.update({note_widget.id: note_widget})
         else:
             note_widget = self.opened_notes[note_id]
-            note_button = self.opened_notes[note_id].note_button
+            note_button = search_button(note_id, self.notes_buttons_list)
+
         note_widget.header_view.textChanged.connect(note_widget.note_button_update)
         self.ui.edit_area.setCurrentWidget(note_widget)
 
-        for button in self.notes:
+        for button in self.notes_buttons_list:
             button.setStyleSheet(open("./styles/note_button.css").read() + "}")
 
         note_button.setStyleSheet(open("./styles/note_button.css").read() + "\n background: rgb(3, 74, 89);\n"
                                                                             "border: 1px solid rgb(255, 255, 255);}")
 
     def hide_menu(self):
-        if self.ui.notes_list.isHidden():
-            self.ui.notes_list.show()
+        if self.ui.scroll_notes_list_area.isHidden():
+            self.ui.scroll_notes_list_area.show()
         else:
-            self.ui.notes_list.hide()
+            self.ui.scroll_notes_list_area.hide()
+
+    def delete_note(self) -> None:
+        yes_no_dialog = QMessageBox(QMessageBox.Information, "Удалить",
+                                    "Вы действительно хотите удалить эту заметку?",
+                                    QMessageBox.Yes | QMessageBox.No)
+        yes_no_dialog.setStyleSheet(open("./styles/QMessageBox.css").read())
+        yes_no_dialog.setIcon(QMessageBox.NoIcon)
+
+        note_id = self.ui.edit_area.currentWidget().id
+
+        if note_id == -1:
+            return
+
+        if QMessageBox.Yes == yes_no_dialog.exec():
+            self.opened_notes.pop(note_id)
+            clear_layout(self.ui.notes_list_layout, NOTES_START_INDEX)
+            empty_edit_area = EmptyEditArea()
+            self.ui.edit_area.addWidget(empty_edit_area)
+            self.ui.edit_area.setCurrentWidget(empty_edit_area)
+
+            request = """DELETE from Notes WHERE id = ?"""
+            self.cursor.execute(request, (note_id, ))
+            self.connection.commit()
+
+            self.notes_buttons_list.clear()
+            self.read_all_notes()
+            print(self.notes_buttons_list)
 
 
 class Note(QWidget):
@@ -128,6 +200,14 @@ class Note(QWidget):
             self.note_button.setText(self.header_view.text())
         else:
             self.note_button.setText(self.header_view.text()[:13] + "...")
+
+
+class EmptyEditArea(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.label = QLabel("Создайте новую заметку или откройте старую")
+        self.label.setStyleSheet("color: rgb(255, 255, 255);\nfont-size: 30px;")
+        self.id = -1
 
 
 if __name__ == '__main__':
--- Projects/Notes/notes_ui.py
+++ Projects/Notes/notes_ui.py
@@ -43,43 +43,34 @@ class Ui_main_widget(object):
 "    height: 45px;\n"
 "    subcontrol-position: top;\n"
 "    subcontrol-origin: margin;\n"
+"}\n"
+"\n"
+"\n"
+"QMessageBox {\n"
+"    background: rgb(70, 94, 116)\n"
 "}")
         self.main_layout = QtWidgets.QGridLayout(main_widget)
         self.main_layout.setContentsMargins(1, 1, 1, 1)
         self.main_layout.setObjectName("main_layout")
-        self.notes_list = QtWidgets.QWidget(main_widget)
-        self.notes_list.setStyleSheet("QWidget#notes_list {\n"
+        self.tool_bar = QtWidgets.QWidget(main_widget)
+        self.tool_bar.setStyleSheet("QWidget {\n"
 "    background: rgb(10, 70, 83);\n"
-"    padding-right: 20px;\n"
 "}\n"
 "\n"
 "QPushButton {\n"
+"    color: rgb(94, 92, 100);\n"
 "    border: none;\n"
-"    border-bottom: 1px solid rgb(4, 29, 38);    \n"
-"    color: rgb(255, 255, 255);\n"
+"    font-size: 30px;\n"
 "    background: rgb(10, 70, 83);\n"
-"    padding: 15px;\n"
 "}\n"
 "\n"
 "QPushButton:hover {\n"
-"    border: 1px solid rgb(255, 255, 255);\n"
+"    color: rgb(237, 51, 59)\n"
 "}")
-        self.notes_list.setObjectName("notes_list")
-        self.notes_list_layout = QtWidgets.QVBoxLayout(self.notes_list)
-        self.notes_list_layout.setContentsMargins(6, 0, 6, 0)
-        self.notes_list_layout.setSpacing(0)
-        self.notes_list_layout.setObjectName("notes_list_layout")
-        self.create_note_button = QtWidgets.QPushButton(self.notes_list)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.create_note_button.sizePolicy().hasHeightForWidth())
-        self.create_note_button.setSizePolicy(sizePolicy)
-        self.create_note_button.setSizeIncrement(QtCore.QSize(0, 0))
-        self.create_note_button.setStyleSheet("")
-        self.create_note_button.setObjectName("create_note_button")
-        self.notes_list_layout.addWidget(self.create_note_button)
-        self.main_layout.addWidget(self.notes_list, 1, 0, 1, 1)
+        self.tool_bar.setObjectName("tool_bar")
+        self.tool_bar_layout = QtWidgets.QHBoxLayout(self.tool_bar)
+        self.tool_bar_layout.setObjectName("tool_bar_layout")
+        self.main_layout.addWidget(self.tool_bar, 0, 1, 1, 1)
         self.menu_button_wrapper = QtWidgets.QWidget(main_widget)
         self.menu_button_wrapper.setStyleSheet("QWidget {\n"
 "    background: rgb(10, 70, 83);\n"
@@ -103,6 +94,39 @@ class Ui_main_widget(object):
         self.menu_button.setObjectName("menu_button")
         self.menu_button_layout.addWidget(self.menu_button)
         self.main_layout.addWidget(self.menu_button_wrapper, 0, 0, 1, 1)
+        self.scroll_notes_list_area = QtWidgets.QScrollArea(main_widget)
+        self.scroll_notes_list_area.setMaximumSize(QtCore.QSize(250, 16777215))
+        self.scroll_notes_list_area.setStyleSheet("QScrollArea {\n"
+"    border: none;\n"
+"}")
+        self.scroll_notes_list_area.setWidgetResizable(True)
+        self.scroll_notes_list_area.setObjectName("scroll_notes_list_area")
+        self.notes_list = QtWidgets.QWidget()
+        self.notes_list.setGeometry(QtCore.QRect(0, 0, 250, 763))
+        self.notes_list.setStyleSheet("QWidget#notes_list {\n"
+"    background: rgb(10, 70, 83);\n"
+"    padding-right: 20px;\n"
+"}\n"
+"\n"
+"QPushButton {\n"
+"    border: none;\n"
+"    border-bottom: 1px solid rgb(4, 29, 38);    \n"
+"    color: rgb(255, 255, 255);\n"
+"    background: rgb(10, 70, 83);\n"
+"    padding: 15px;\n"
+"}\n"
+"\n"
+"QPushButton:hover {\n"
+"    border: 1px solid rgb(255, 255, 255);\n"
+"}")
+        self.notes_list.setObjectName("notes_list")
+        self.notes_list_layout = QtWidgets.QVBoxLayout(self.notes_list)
+        self.notes_list_layout.setObjectName("notes_list_layout")
+        self.create_note_button = QtWidgets.QPushButton(self.notes_list)
+        self.create_note_button.setObjectName("create_note_button")
+        self.notes_list_layout.addWidget(self.create_note_button)
+        self.scroll_notes_list_area.setWidget(self.notes_list)
+        self.main_layout.addWidget(self.scroll_notes_list_area, 1, 0, 1, 1)
         self.edit_area = QtWidgets.QStackedWidget(main_widget)
         self.edit_area.setStyleSheet("QStackedWidget {\n"
 "    background: rgb(3, 74, 89);\n"
@@ -131,15 +155,6 @@ class Ui_main_widget(object):
         self.__.setObjectName("__")
         self.edit_area.addWidget(self.__)
         self.main_layout.addWidget(self.edit_area, 1, 1, 1, 1)
-        self.tool_bar = QtWidgets.QWidget(main_widget)
-        self.tool_bar.setStyleSheet("QWidget#notes_list {\n"
-"    background: rgb(10, 70, 83);\n"
-"}\n"
-"")
-        self.tool_bar.setObjectName("tool_bar")
-        self.tool_bar_layout = QtWidgets.QHBoxLayout(self.tool_bar)
-        self.tool_bar_layout.setObjectName("tool_bar_layout")
-        self.main_layout.addWidget(self.tool_bar, 0, 1, 1, 1)
 
         self.retranslateUi(main_widget)
         QtCore.QMetaObject.connectSlotsByName(main_widget)
@@ -147,6 +162,6 @@ class Ui_main_widget(object):
     def retranslateUi(self, main_widget):
         _translate = QtCore.QCoreApplication.translate
         main_widget.setWindowTitle(_translate("main_widget", "Form"))
-        self.create_note_button.setText(_translate("main_widget", "..."))
         self.menu_button.setText(_translate("main_widget", "☰"))
+        self.create_note_button.setText(_translate("main_widget", "PushButton"))
         self._.setWhatsThis(_translate("main_widget", "<html><head/><body><p>dasf</p></body></html>"))
